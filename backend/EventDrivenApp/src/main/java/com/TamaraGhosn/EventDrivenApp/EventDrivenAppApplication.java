0f374a496f8953cc8e96ea686d81749bddcaa35c
tamaraghosn <tamaraghosn01@gmail.com>
2025-11-21 15:35:28 +0200

    first commit

---
+++ backend/EventDrivenApp/src/main/java/com/TamaraGhosn/EventDrivenApp/EventDrivenAppApplication.java
@@ -0,0 +1,13 @@
+package com.TamaraGhosn.EventDrivenApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenAppApplication.class, args);
+	}
+
+}
---
+++ backend/EventDrivenApp/src/test/java/com/TamaraGhosn/EventDrivenApp/EventDrivenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TamaraGhosn.EventDrivenApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
